3313a632cfd72ea3fc19b20c8843fc5877fd4327
Tymon <tymekcoke@gmail.com>
2025-02-06 23:07:04 +0100

    Dodanie menu, wgrywanie klas z pliku do programu.

--- main.c
+++ main.c
@@ -35,12 +35,15 @@ void dodajZadanieDoPliku(Zadanie zadania[], int *ilosc_zadan, const char *nazwa_
 
     printf("Podaj priorytet (1-wysoki, 2-sredni, 3-niski): ");
     scanf("%d", &zadanie.priorytet);
-    getchar();
+    getchar(); 
 
     fprintf(plik, "%s|%s|%d\n", zadanie.tytul, zadanie.opis, zadanie.priorytet);
     fclose(plik);
 
-    printf("Zadanie zapisane do pliku.\n");
+    zadania[*ilosc_zadan] = zadanie;
+    (*ilosc_zadan)++;
+
+    printf("Zadanie zapisane do pliku i dodane do listy.\n");
 }
 
 void wgrajZadania(Zadanie zadania[], int *ilosc_zadan, const char *nazwa_pliku) {
@@ -65,13 +68,56 @@ void wgrajZadania(Zadanie zadania[], int *ilosc_zadan, const char *nazwa_pliku) 
     printf("Załadowano %d zadań z pliku.\n", *ilosc_zadan);
 }
 
+void wyswietlZadania(Zadanie zadania[], int ilosc_zadan) {
+    if (ilosc_zadan == 0) {
+        printf("Brak zapisanych zadań.\n");
+        return;
+    }
+
+    printf("\nLista zadań:\n");
+    for (int i = 0; i < ilosc_zadan; i++) {
+        printf("Zadanie %d:\n", i + 1);
+        printf("  Tytuł: %s\n", zadania[i].tytul);
+        printf("  Opis: %s\n", zadania[i].opis);
+        printf("  Priorytet: %d\n\n", zadania[i].priorytet);
+    }
+}
+
 int main() {
     Zadanie *zadania = malloc(MAX_ZADANIA * sizeof(Zadanie));
+    if (zadania == NULL) {
+        printf("Błąd alokacji pamięci!\n");
+        return 1;
+    }
+
     int ilosc_zadan = 0;
 
     wgrajZadania(zadania, &ilosc_zadan, "zadania.txt");
 
-    dodajZadanieDoPliku(zadania, &ilosc_zadan, "zadania.txt");
+    int wybor;
+    do {
+        printf("\nMENU:\n");
+        printf("1. Dodaj zadanie\n");
+        printf("2. Wyświetl zadania\n");
+        printf("3. Wyjdź\n");
+        printf("Twój wybór: ");
+        scanf("%d", &wybor);
+        getchar(); 
+
+        switch (wybor) {
+            case 1:
+                dodajZadanieDoPliku(zadania, &ilosc_zadan, "zadania.txt");
+                break;
+            case 2:
+                wyswietlZadania(zadania, ilosc_zadan);
+                break;
+            case 3:
+                printf("Zamykanie programu...\n");
+                break;
+            default:
+                printf("Niepoprawny wybór! Spróbuj ponownie.\n");
+        }
+    } while (wybor != 3);
 
     free(zadania);
     return 0;
